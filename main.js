6bf25f97e5c4075dfe9d8c5c508eed0b99ee8198
Guilherme Alves Lopes <64388197+G-370@users.noreply.github.com>
2020-07-13 08:33:57 -0300

    hotfixn3
    Oh my

--- main.js
+++ main.js
@@ -7,7 +7,5 @@ function calcular() {
         let vmp = v_colocar / v_c / 60;
 	let v_minutos = Math.trunc(v_colocar / v_c / 60);
 	let v_segundos = Math.trunc((vmp - Math.floor(vmp))*60);
-	alert(`${v_minutos}min${v_segundos}s`);
-        alert(vmp);
-        alert((vmp - Math.floor(vmp))*60);
+	alert(`${v_minutos}min${v_segundos}s \n ${vmp - Math.floor(vmp))*60} \n ${vmp}`);
 }
