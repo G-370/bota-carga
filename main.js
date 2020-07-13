df0caf169bc810eebf82be20bf7476ef3b6757fd
Guilherme Alves Lopes <64388197+G-370@users.noreply.github.com>
2020-07-13 08:23:48 -0300

    hotfix
    Hotfix do calculo dos segundos

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ function calcular() {
 	let v_pb = document.getElementById('pesobruto').value;
 	let v_c = document.getElementById('carga').value;
 	let v_colocar = v_pb - v_t;
+        let vmp = v_colocar / v_c / 60;
 	let v_minutos = Math.trunc(v_colocar / v_c / 60);
-	let v_segundos = Math.trunc((( v_colocar / v_c ) % 1)*60);
+	let v_segundos = Math.trunc((vmp - Math.floor(vmp))*60);
 	alert(`${v_minutos}min${v_segundos}s`);
 	
 }
