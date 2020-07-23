255d6102f80e052449363022e00d2ed2e5a53958
G-370 <guilhermealvesopes370@gmail.com>
2020-07-23 06:56:07 -0300

    Revert "Added animation on change." And never do development during lunchbreak. Whatever I think will work and deploy during break, wont, as it has never, development requires time and thought.
    This reverts commit 08ab5c947ad20de2bf3c4c06a2b4b36de842112a.

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-var min = document.getElementById("m");
-var sec = document.getElementById("s");
-
 function setms(m, s) {
    m = (typeof m === "number") ? Math.trunc(m) : m;
    s = (typeof s === "number") ? Math.trunc(s) : s;
-   min.textContent = `${m} min`;
-   sec.textContent = `${s} s`;
+   document.getElementById("m").textContent = `${m} min`;
+   document.getElementById("s").textContent = `${s} s`;
 }
 
 function calcular() {
@@ -17,19 +14,7 @@ function calcular() {
    let minutos = total_segundos / 60;
    let segundos = (minutos - Math.trunc(minutos)) * 60;
    setms(minutos, segundos);
-   piscatempo();
    //DEBUG
    //alert(`tara:${tara}\nbruto:${bruto}\ncarga:${carga}\nliquido:${liquido}\ntotal_segundos:${total_segundos}\nMinutos: ${minutos}\nSegundos: ${segundos}`);
 
-}
-
-function piscatempo(){
-   min.style.color = "#77FF00";
-   sec.style.color = "#77FF00";
-   setTimeout(
-     () => {
-       min.style.color = "white";
-       sec.style.color = "white"
-     }, 900
-     );
 }
